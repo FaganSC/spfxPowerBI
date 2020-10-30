7e577550d4cd045d3280406b981e17aea6933490
Shawn Fagan (Marathon) <sfagan@marathonus.com>
2020-10-30 12:45:04 -0400

    Initial Commit

---
+++ src/webparts/powerBi/PowerBiWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'PowerBiWebPartStrings';
+import PowerBi from './components/PowerBi';
+import { IPowerBiProps } from './components/IPowerBiProps';
+
+export interface IPowerBiWebPartProps {
+  description: string;
+}
+
+export default class PowerBiWebPart extends BaseClientSideWebPart<IPowerBiWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IPowerBiProps> = React.createElement(
+      PowerBi,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
